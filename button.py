8953fa281c46a07b2394bd25f250f3d8365cbb25
Ashom-Tettey Dennis Dela <ashomtetteydennisdela@gmail.com>
2018-07-17 15:59:10 +0000

    Add files via upload

--- button.py
+++ button.py
@@ -8,20 +8,18 @@ from time import sleep
 
 
 
- 
+
 def cam():
 
-     
     print("About to take a picture.")
     with picamera.PiCamera() as camera:
     #camera = picamera.PiCamera()
         camera.resolution = (1280,720)
 	id = str(time.time())
-        name = "/home/pi/Desktop/cookies/image_" + id +   ".jpg"
+        name = "/home/pi/Desktop/cookies/image_" + id + ".jpg"
 	camera.capture(name)
     print("picture taken")
-   
-	
+
 
 
 led = LED(17)
